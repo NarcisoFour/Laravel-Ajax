ed815fe8116aa0f660d20943112a7852725dd1d3
NarcisoFour <NarcisoFour@gmail.com>
2018-01-02 14:39:36 -0600

    Listado de productos y paginación

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,33 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Panel Administrativo</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <p>
+                        {{ $productos->total() }} Registros |
+                        página {{ $productos->currentPage() }}
+                        de {{ $productos->lastPage() }}
+                    </p>
+                    <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th width="20px">ID</th>
+                                <th>Nombre del Producto</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($productos as $item)
+                                <tr>
+                                    <td width="20px">{{ $item->id }}</td>
+                                    <td>{{ $item->nombre }}</td>
+                                    <td width="20px"></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {!! $productos->render() !!}
                 </div>
             </div>
         </div>
