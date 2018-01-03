dc83eabefb95c8eb376a3757c4e86560614d9d58
NarcisoFour <NarcisoFour@gmail.com>
2018-01-02 18:40:05 -0600

    Guard

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,7 +6,11 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        Model::unguard();
+
         $this->call(UsersTableSeeder::class);
         $this->call(ProductoTableSeeder::class);
+
+        Model::reguard();
     }
 }
